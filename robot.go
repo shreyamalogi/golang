4c9d11dbe6495633e66a80ec125a3a7d261e47a4
Shreya Christiana <5hre9a@gmail.com>
2021-06-13 23:32:22 +0530

    Create robot.go

---
+++ robot.go
@@ -0,0 +1,39 @@
+package main
+import "fmt"
+
+func main() {
+    //your code goes here
+	for i := 1; i<=3; i++{
+     
+     var i int
+     fmt.Scanln(&i)
+    
+
+		switch i {
+		case 0:
+			fmt.Println("Zero")
+		case 1:
+			fmt.Println("One")
+		case 2:
+			fmt.Println("Two")
+		case 3:
+			fmt.Println("Three")
+		case 4:
+			fmt.Println("Four")
+		case 5:
+			fmt.Println("Five")
+		case 6:
+			fmt.Println("Six")
+		case 7:
+			fmt.Println("Seven")
+		case 8:
+			fmt.Println("Eight")
+		case 9:
+			fmt.Println("Nine")
+		case 10:
+			fmt.Println("Ten")
+  default:
+      fmt.Println(i)
+		}
+	}
+}
