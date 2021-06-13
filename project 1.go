dc79443b124130bf4c98922edf1fec8ca63aa262
Shreya Christiana <5hre9a@gmail.com>
2021-06-13 23:38:54 +0530

    Create project 1.go

---
+++ project 1.go
@@ -0,0 +1,19 @@
+// Debug & Fix
+
+
+// Your colleague tried to make a program that is intended to output "GO" three times on separate lines.
+// However, he is still new to Go and has made a number of mistakes in the code.
+
+// Debug and fix the code to produce the desired output.
+
+//debugged code
+
+package main
+import "fmt"
+
+func main() {
+   // outputs GO 3 times
+    fmt.Println("GO")
+    fmt.Println("GO")
+    fmt.Println("GO")
+}
